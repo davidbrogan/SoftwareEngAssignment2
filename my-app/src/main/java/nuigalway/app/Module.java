5d98cd98b074c898bc260deecd42831ea90f4bd7
David Brogan <brogan9@gmail.com>
2019-11-13 15:18:53 +0000

    Created classes, fiinished Student

---
+++ my-app/src/main/java/nuigalway/app/Module.java
@@ -0,0 +1,5 @@
+package nuigalway.app;
+
+public class Module {
+
+}
---
+++ my-app/src/main/java/nuigalway/app/Student.java
@@ -0,0 +1,67 @@
+package nuigalway.app;
+
+import java.util.List;
+
+public class Student {
+	private String name;
+	private int age;
+	private String dob;
+	private String id;
+	private List<Course> courseList;
+	public Student()
+	{
+		
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<Course> getCourseList() {
+		return courseList;
+	}
+
+	public void setCourseList(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+
+	public List<Module> getModuleList() {
+		return moduleList;
+	}
+
+	public void setModuleList(List<Module> moduleList) {
+		this.moduleList = moduleList;
+	}
+
+	private List<Module> moduleList;
+	
+	
+
+}
